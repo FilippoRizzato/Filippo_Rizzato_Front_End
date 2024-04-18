f3cca463e7379d129f323e17d156b52552682c14
FilippoRizzato <94640711+FilippoRizzato@users.noreply.github.com>
2024-04-18 18:44:16 +0200

    Delete Filippo_Rizzato_Front_End-main/Filippo_CRUD_Javascript/Filippo_CRUD_JS directory

--- Filippo_Rizzato_Front_End-main/Filippo_CRUD_Javascript/Filippo_CRUD_JS/backend/insert.sql
+++
@@ -1,6 +0,0 @@
-insert into rizzato_filippo_API.products(nome, prezzo, marca)
-values ("tosaerba", 289.99, "oleomac"),
-       ("vaso", 9.99, "Villeroy e Boch"),
-       ("personal computer", 1799.99, "msi"),
-       ("bibbia", 15, "san paolo"),
-       ("modellino ferrari", 129.99, "Ferrari");
